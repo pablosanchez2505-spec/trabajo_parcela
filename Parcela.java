4d230fcf0f13d759f8ad14d9af191dca16317c1b
Pablo <pablo.sanchez.2505@gmail.com>
2026-04-14 19:21:38 +0200

    terminada la clase parcela

--- Parcela.java
+++ Parcela.java
@@ -1,12 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import figura.Medible;
+
 public class Parcela {
-   public void Añadir_figura(){
+private String nombre;
+private List<Medible> medibles;
 
+public Parcela(String nombre) {
+   medibles = new ArrayList<>();
+   this.nombre =nombre;
+}
+public List<Medible> getMedibles() {
+    return medibles;
+}
+public void setMedibles(List<Medible> medibles) {
+    this.medibles = medibles;
+}
+   public void añadir_figura(Medible figura){
+   medibles.add(figura);
    }
-   public void area_total(){
-
-
+   public double area_total(){
+    double total=0;
+    for(Medible figura:medibles){
+        total=total+figura.calcularArea();
+    }
+    return total;
    }
-   public void perimetro_total(){
-
+   public double perimetro_total(){
+    double total=0;
+    for(Medible figura:medibles){
+        total=total+figura.calcularPerimetro();
+    }
+    return total;
+   }
+   public void resumen(){
+    System.out.println("Nombre parcela: "+nombre);
+    int i=0;
+    System.out.println("Número total de figuras: "+medibles.size());
+     for(Medible figura:medibles){
+        i++;
+        System.out.println("Figura "+i+":"+figura.nombre());
+    }
+    System.out.println("Area total: "+area_total());
+    System.out.println("Perimetro total: "+perimetro_total());
    }
 }
--- main.java
+++ main.java
@@ -1,12 +1,16 @@
+import figura.Circulo;
+import figura.Cuadrado;
+import figura.Medible;
+
 public class Main {
 
-public void main(String[] args) {
-    
-
-
-
-
-
+public void main(String[] args) {    
+Parcela parcela1 = new Parcela("parcela1");
+ Medible circulo1 = new Circulo(3);
+ Medible cuadrado1= new Cuadrado(2.4); 
+parcela1.añadir_figura(cuadrado1);
+parcela1.añadir_figura(circulo1);
+parcela1.resumen();
 
 
 }   
